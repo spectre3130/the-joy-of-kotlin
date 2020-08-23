2deedbab6228216e8d57f6d157297de481404003
spectre3130 <flqktm123@gmail.com>
2020-08-24 01:21:58 +0900

    5장 concat, reverse, sum, variance

--- src/org/kotlin/example/chap05/List.kt
+++ src/org/kotlin/example/chap05/List.kt
@@ -2,9 +2,9 @@ package org.kotlin.example.chap05
 
 import java.lang.IllegalStateException
 
-sealed class List<A> {
+sealed class List<out A> {
     abstract fun isEmpty(): Boolean
-    abstract fun setHead(a: A): List<A>
+    abstract fun setHead(a: @UnsafeVariance A): List<A>
 
     private object Nil: List<Nothing>() {
         override fun isEmpty() = true
@@ -24,9 +24,13 @@ sealed class List<A> {
             }
     }
 
-    fun cons(a: A): List<A> = Cons(a, this)
+    fun cons(a: @UnsafeVariance A): List<A> = Cons(a, this)
 
     fun drop(n: Int): List<A> = drop(this, n)
+
+    fun concat(list: List<@UnsafeVariance A>): List<A> = concat(this, list)
+
+    fun reverse(): List<A> = reverse(invoke(), this)
 
 //    fun drop(n: Int): List<A> {
 //        tailrec fun drop(n: Int, list: List<A>): List<A> =
@@ -46,6 +50,21 @@ sealed class List<A> {
         tailrec fun <A> dropWhile(list: List<A>, p: (A) -> Boolean): List<A> = when (list) {
             Nil -> list
             is Cons -> if (p(list.head)) list else dropWhile(list.tail, p)
+        }
+
+        fun <A> concat(list1: List<A>, list2: List<A>): List<A> = when (list1) {
+            Nil -> list2
+            is Cons -> Cons(list1.head, concat(list1.tail, list2))
+        }
+
+        tailrec fun <A> reverse(acc: List<A>, list: List<A>): List<A> = when (list) {
+            Nil -> acc
+            is Cons -> reverse(acc.cons(list.head), list.tail)
+        }
+
+        fun sum(ints: List<Int>): Int = when (ints) {
+            Nil -> 0
+            is Cons -> ints.head + sum(ints.tail)
         }
 
         operator
--- src/org/kotlin/example/chap05/Solution05.kt
+++ src/org/kotlin/example/chap05/Solution05.kt
@@ -8,4 +8,9 @@ fun main() {
 //    println(list.drop(3))
     println(List.drop(list, 3))
     println(list.drop(4).setHead(0))
+    val list1 = List('a', '_', 'l', 'o', 'n', 'g')
+    val list2 = List('a', '_', 'l', 'i', 's', 't')
+    println(list1.concat(list2.drop(1)))
+    println(list.reverse().drop(1).reverse())
+    println(List.sum(list))
 }
