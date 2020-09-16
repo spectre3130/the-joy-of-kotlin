72dbfa27054f4106233b7b7b4363cd362254a44e
spectre3130 <flqktm123@gmail.com>
2020-09-17 00:41:19 +0900

    6장 Option 마무리

--- src/org/kotlin/example/chap06/Option.kt
+++ src/org/kotlin/example/chap06/Option.kt
@@ -1,5 +1,8 @@
 package org.kotlin.example.chap06
 
+import org.kotlin.example.chap05.List
+import kotlin.math.pow
+
 sealed class Option<out A> {
     abstract fun isEmpty(): Boolean
 //    abstract fun <B> map(f: (A) -> B): Option<B>
@@ -27,12 +30,18 @@ sealed class Option<out A> {
         is Some -> value
     }
 
+    fun orElse(default: () -> Option<@UnsafeVariance A>): Option<A> =
+        map { _ -> this }.getOrElse(default)
+
+    fun filter(p: (A) -> Boolean): Option<A> =
+        flatMap { if (p(it)) this else None }
+
     fun <B> map(f: (A) -> B): Option<B> = when (this) {
         is None -> None
         is Some -> Some(f(value))
     }
 
-    fun <B> flatMap(f: (A)-> Option<B>): Option<B> = map(f).getOrElse{ None }
+    fun <B> flatMap(f: (A) -> Option<B>): Option<B> = map(f).getOrElse{ None }
 
     companion object {
         operator fun <A> invoke(a: A? = null): Option<A> = when (a) {
@@ -40,8 +49,83 @@ sealed class Option<out A> {
             else -> Some(a)
         }
 
-        fun max(list: List<Int>): Option<Int> = Option(list.max())
+        fun max(list: kotlin.collections.List<Int>): Option<Int> = Option(list.max())
 
         fun getDefault(): Int = throw RuntimeException()
+
+        fun <A, B, C> map2(
+                oa: Option<A>,
+                ob: Option<B>,
+                f: (A) -> (B) -> C): Option<C> =
+                oa.flatMap { a ->
+                    ob.map { b ->
+                        f(a)(b)
+                    }
+                }
+
+        fun <A, B, C, D> map3(
+                oa: Option<A>,
+                ob: Option<B>,
+                oc: Option<C>,
+                f: (A) -> (B) -> (C) -> D): Option<D> =
+                oa.flatMap { a ->
+                    ob.flatMap { b ->
+                        oc.map { c ->
+                            f(a)(b)(c)
+                        }
+                    }
+                }
+
+//        fun <A> sequence(list: List<Option<A>>): Option<List<A>> =
+//                list.foldRight(Option(List())) { x ->
+//                    { y: Option<List<A>> -> map2(x, y) { a ->
+//                        { b: List<A> -> b.cons(a) } }
+//                    }
+//                }
+
+        fun <A, B> traverse(list: List<A>, f: (A) -> Option<B>): Option<List<B>> =
+                list.foldRight(Option(List())) { x ->
+                    { y: Option<List<B>> ->
+                        map2(f(x), y) { a ->
+                            { b: List<B> ->
+                               b.cons(a)
+                            }
+                        }
+                    }
+                }
+
+        fun <A> sequence(list: List<Option<A>>): Option<List<A>> =
+                traverse(list) { x -> x }
+    }
+}
+
+fun <A, B> lift(f: (A) -> B): (Option<A>) -> Option<B> = {
+    try {
+        it.map(f)
+    } catch (e: Exception) {
+        Option()
+    }
+}
+
+fun <A, B> hLift(f: (A) -> B):(A) -> Option<B> = {
+    try {
+        Option(it).map(f)
+    } catch (e: Exception) {
+        Option()
+    }
+}
+
+val mean: (kotlin.collections.List<Double>) -> Option<Double> = { list ->
+    when {
+        list.isEmpty() -> Option()
+        else -> Option(list.sum() / list.size)
+    }
+}
+
+val variance: (kotlin.collections.List<Double>) -> Option<Double> = { list ->
+    mean(list).flatMap { m ->
+        mean(list.map { x ->
+            (x - m).pow(2.0)
+        })
     }
 }
--- src/org/kotlin/example/chap06/Solution06.kt
+++ src/org/kotlin/example/chap06/Solution06.kt
@@ -1,11 +1,23 @@
 package org.kotlin.example.chap06
 
+import org.kotlin.example.chap05.List
 import org.kotlin.example.chap06.Option.Companion.getDefault
 import org.kotlin.example.chap06.Option.Companion.max
 
 fun main() {
     val max1 = max(listOf(3, 5, 7, 2, 1)).getOrElse(::getDefault)
     println(max1)
-    val max2 = max(listOf()).getOrElse(::getDefault)
-    println(max2)
+//    val max2 = max(listOf()).getOrElse(::getDefault)
+//    println(max2)
+
+    val parseWithRadix: (Int) -> (String) -> Int = { radix ->
+        { string ->
+            Integer.parseInt(string ,radix)
+        }
+    }
+
+    val parse16 = hLift(parseWithRadix(16))
+    val list = List("4", "5", "6", "7", "8", "9", "A", "B")
+    val result = Option.sequence(list.map(parse16))
+    println(result)
 }
