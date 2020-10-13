70efc17bad54189e5a627ed78f34a0285d6a3b49
spectre3130 <flqktm123@gmail.com>
2020-10-14 00:49:27 +0900

    7장 Either 추가

---
+++ src/org/kotlin/example/chap07/Either.kt
@@ -0,0 +1,24 @@
+package org.kotlin.example.chap07
+
+sealed class Either<E, out A> {
+    abstract fun <B> map(f: (A) -> B): Either<E, B>
+
+    internal class Left<E, out A>(private val value: E): Either<E, A>() {
+
+        override fun <B> map(f: (A) -> B): Either<E, B> = Left(value)
+
+        override fun toString(): String = "Left($value)"
+    }
+
+    internal class Right<E, out A>(private val value: A): Either<E, A>() {
+
+        override fun <B> map(f: (A) -> B): Either<E, B> = Right(f(value))
+
+        override fun toString(): String = "Right($value)"
+    }
+
+    companion object {
+        fun <E, A> left(value: E): Either<E, A> = Left(value)
+        fun <E, B> right(value: B): Either<E, B> = Right(value)
+    }
+}
---
+++ src/org/kotlin/example/chap07/Solution07.kt
@@ -0,0 +1,5 @@
+package org.kotlin.example.chap07
+
+fun main() {
+
+}
